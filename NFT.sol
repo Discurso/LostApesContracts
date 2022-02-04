45f894ef98a4b844b7daf9b03afeee899f159efe
Ahmed Castro <ahmed.hn.43@gmail.com>
2022-02-04 14:03:44 -0500

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// whitelist
+// se cobra en eth
+
 contract Minter {
     function register(uint token_id) public
     {
