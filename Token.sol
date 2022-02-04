403c603814291f982426d06889e88f49c526db97
Ahmed Castro <ahmed.hn.43@gmail.com>
2022-02-03 23:53:13 -0600

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -2,23 +2,22 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyERC20 is ERC20 {
-  address public minter = 0x0000000000000000000000000000000000000000;
+contract MyERC20 is ERC20, Ownable {
+    address public minter = 0x0000000000000000000000000000000000000000;
 
-  constructor () ERC20("My Token", "TKN") {
-    _mint(msg.sender, 1000000 ether);
-  }
+    constructor () ERC20("My Token", "TKN") {
+    }
 
-  function setMinter(address new_minter) public
-  {
-      require(msg.sender == minter, "Must be minter");
-      minter = new_minter;
-  }
+    function setMinter(address new_minter) public onlyOwner
+    {
+        minter = new_minter;
+    }
 
-  function mintReward(address beneficiary, uint amount) public
-  {
-      require(msg.sender == minter, "Must be minter");
-      _mint(beneficiary, amount);
-  }
+    function mintReward(address beneficiary, uint amount) public
+    {
+        require(msg.sender == minter, "Must be minter");
+        _mint(beneficiary, amount);
+    }
 }
